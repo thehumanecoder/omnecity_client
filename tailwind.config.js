1798bc7077732a4bb91e8b8f8e0019dd6e6daffd
Biswas Sampad <biswassampads@gmail.com>
2021-06-08 21:05:50 +0530

    basic coding started and best setup

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    purge: [
+        "./pages/**/*.{js,jsx}",
+        "./components/**/*.{js,jsx}"
+
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
